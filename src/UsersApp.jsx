2d888eb5900b1978b424095cc47a31663acc58c1
jhon patiño <jhon@gmail.com>
2024-04-03 00:42:02 -0500

    se creo la carpeta componentes, y se creo cada archivo para aplicar el maquetado del listado

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,8 +1,29 @@
+import { UserForm } from "./components/UserForm";
+import { UsersList } from "./components/UsersList";
+
 export const UsersApp =()=>{
+   
+   const initialUsers=[
+      {
+        id:1,
+        username:'pepe',
+        password:'12345',
+        email:'pepito@gmail.com'
+      },
+   ];
+   
     return (
         <>
             <div className="container my-4 ">
                 <h2>Users App</h2>
+                <div className="row">
+                    <div className="col">
+                       <UserForm/>
+                    </div>
+                        <div className="col">
+                            <UsersList users={ initialUsers}/>
+                       </div>
+                </div>
             </div>
         </>
     );
